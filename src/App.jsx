8ee0eeadc9526f035141ac34a6d9d3ad1fa123d3
leeosilvp <leo965679@gmail.com>
2025-11-10 18:40:12 -0300

    a- criando e estilizando pagina de erro

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Feed from "./routes/Feed"
 import Profile from "./routes/Profile"
 import Header from "./components/Header"
+import Icon from "./components/Icon"
+import Error from "./routes/Error"
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Navigate to="/feed" replace />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
+      <Icon />
     </BrowserRouter>
 
   )
---
+++ src/components/Icon.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/svg/logo-light.svg'
+
+const Icon = () => {
+  return (
+    <article className='btn-chat-eloy'>
+        <img src={logo} title="Conversar com eloy" />
+    </article>
+  )
+}
+
+export default Icon
