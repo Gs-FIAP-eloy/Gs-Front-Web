12b576c34eaff367bdd483da25197cc8559320e2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-13 11:43:31 -0300

    a- criando conteudo do chat

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -8,6 +8,46 @@ const Chat = () => {
         <section className="ctn-header-chat">
           <h1>Mensagens</h1>
         </section>
+        <section className="content-chat">
+          <aside className="ctn-aside-chat">
+            <article className="profile-user-chat">
+              <section className="img-profile-chat">
+                <img src="https://avatars.githubusercontent.com/u/225023097?v=4" />
+              </section>
+              <section className="ctn-info-profile-chat">
+                <section className="info-profile-chat">
+                  <h1>Samuel Monteiro</h1>
+                  <section className="preview-message-chat">
+                    <i className="fa-solid fa-check-double"></i>
+                    <p>•</p>
+                    <h2>Oi Léo! Posso sim. o q...</h2>
+                  </section>
+                </section>
+                <button><i className="fa-solid fa-ellipsis"></i></button>
+              </section>
+            </article>
+
+          </aside>
+          <section className="content-active-chat">
+            <section className="header-active-chat">
+              <section className="img-profile-chat">
+                <img src="https://avatars.githubusercontent.com/u/225023097?v=4" />
+              </section>
+              <h1>Samuel Monteiro</h1>
+            </section>
+            <section className="content-message">
+              <article className="message-user">
+                <p>Eae Samuel beleza! pode me ajudar em uma tarefa?</p>
+              </article>
+              <article className="message-received">
+                <p>Oi Léo! Posso sim. o que você precisa?</p>
+              </article>
+            </section>
+            <section className="ctn-send-message">
+              
+            </section>
+          </section>
+        </section>
       </section>
       <aside className="right">
         <CardNewslatter />
