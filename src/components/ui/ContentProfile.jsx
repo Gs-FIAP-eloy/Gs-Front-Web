9f6d5a76080ed3532580a2a14526f9e352476099
leeosilvp <leo965679@gmail.com>
2025-11-14 16:19:00 -0300

    m- trazendo dados do json do usuario logado para o perfil

--- src/components/ui/ContentProfile.jsx
+++ src/components/ui/ContentProfile.jsx
@@ -1,28 +1,50 @@
+import { useEffect, useState } from "react";
 
 const ContentProfile = () => {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const user = localStorage.getItem("eloy_user");
+        if (!user) return;
+
+        const usuarioLogado = JSON.parse(user);
+
+        fetch("/db/users.json")
+            .then(res => res.json())
+            .then(data => {
+                if (Array.isArray(data)) {
+                    const foundUser = data.find(u => u.id === usuarioLogado.id);
+                    if (foundUser) setUserData(foundUser);
+                }
+            })
+            .catch(err => console.error("Erro ao carregar JSON:", err));
+    }, []);
+
+    if (!userData) return null;
+
     return (
         <section className="content-profile">
             <article className="img-profile">
-                <img src="https://media.licdn.com/dms/image/v2/D4D03AQFnlrLQvobGTQ/profile-displayphoto-scale_400_400/B4DZmrHQWgJMAk-/0/1759512430533?e=1764201600&v=beta&t=wUtGBDNXXaVi-F_42kNW6syFUSWFhdQJsucgMH1kw08" />
+                <img src={userData.foto} alt={userData.nome} />
             </article>
             <section className="content-info-profile">
                 <section className="left-content-profile">
-                    <h1 className="user-name">Leonardo Silva</h1>
-                    <h1 className="title-profile">Software Engineer | Back-end Developer | Python | JavaScript | React | Node.Js | MySQL | Agile Methodologies</h1>
-                    <h1 className="location">São Paulo • Brasil</h1>
+                    <h1 className="user-name">{userData.nome}</h1>
+                    <h1 className="title-profile">{userData.titulo}</h1>
+                    <h1 className="location">{userData.estado} • {userData.pais}</h1>
                     <section className="following-followers">
-                        <h1>0 seguidores</h1>
+                        <h1>{userData.seguidores} seguidores</h1>
                         <h1>•</h1>
-                        <h1>0 seguindo</h1>
+                        <h1>{userData.seguindo} seguindo</h1>
                     </section>
                 </section>
 
                 <section className="right-content-profile">
                     <a href="https://www.fiap.com.br/" target="_blank" className="card-job">
-                        <img src="https://avatars.githubusercontent.com/u/79948663?s=200&v=4" />
+                        <img src='https://avatars.githubusercontent.com/u/79948663?s=200&v=4' alt={userData.empresa} />
                         <section className="position-company">
-                            <h1>FIAP</h1>
-                            <h2>Engenheiro de Software</h2>
+                            <h1>{userData.empresa}</h1>
+                            <h2>{userData.cargo}</h2>
                         </section>
                     </a>
                 </section>
@@ -33,7 +55,7 @@ const ContentProfile = () => {
                 <button>Compartilhar perfil</button>
             </section>
         </section>
-    )
-}
+    );
+};
 
-export default ContentProfile
+export default ContentProfile;
