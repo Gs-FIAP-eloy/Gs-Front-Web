32de4c52e04abe1d8bb0c1ace16f91a089dd9aef
leeosilvp <leo965679@gmail.com>
2025-11-14 14:27:07 -0300

    m- trazendo banner do json do usuario logado no BannerProfile

--- src/components/ui/BannerProfile.jsx
+++ src/components/ui/BannerProfile.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 
 const BannerProfile = () => {
+
+  const [banner, setBanner] = useState("");
+
+  useEffect(() => {
+    const user = localStorage.getItem("eloy_user");
+    if (!user) return;
+
+    const usuarioLogado = JSON.parse(user);
+
+    fetch("/db/users.json")
+      .then(res => res.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          const userData = data.find(u => u.id === usuarioLogado.id);
+
+          if (userData?.banner) {
+            setBanner(userData.banner);
+          }
+        }
+      })
+      .catch(err => console.error("Erro ao carregar JSON:", err));
+  }, []);
+
   return (
     <section className="banner-profile">
-      <img src="https://media.licdn.com/dms/image/v2/D4D16AQGjobWVHrk20Q/profile-displaybackgroundimage-shrink_200_800/B4DZoj8pleGQAU-/0/1761539704579?e=1764201600&v=beta&t=lbktxK-vhCcI3XGdqkfBVUyN4nTgVyOON0uBIvNgXdg" />
+      <img src={banner} />
     </section>
-  )
-}
+  );
+};
 
-export default BannerProfile
+export default BannerProfile;
