c8d09207f7a7e7e139e31e6a9b1b5b4d9c88d764
leeosilvp <leo965679@gmail.com>
2025-11-14 14:37:15 -0300

    m- trazendo dados do json do usuario logado para CardAcademic

--- src/components/ui/CardAcademic.jsx
+++ src/components/ui/CardAcademic.jsx
@@ -1,24 +1,49 @@
+import { useEffect, useState } from "react";
+import HeaderCard from "./HeaderCard";
 
 const CardAcademic = () => {
+
+    const [formacao, setFormacao] = useState([]);
+
+    useEffect(() => {
+        const user = localStorage.getItem("eloy_user");
+        if (!user) return;
+
+        const usuarioLogado = JSON.parse(user);
+
+        fetch("/db/users.json")
+            .then(res => res.json())
+            .then(data => {
+                if (Array.isArray(data)) {
+                    const userData = data.find(u => u.id === usuarioLogado.id);
+
+                    if (userData?.formacao) {
+                        setFormacao(userData.formacao);
+                    }
+                }
+            })
+            .catch(err => console.error("Erro ao carregar JSON:", err));
+    }, []);
+
     return (
         <section className="ctn-card">
-            <section className="header-card">
-                <h1>Formação acadêmica</h1>
-                <section className="btns-header-card">
-                    <button><i className="fa-solid fa-plus"></i></button>
-                    <button><i className="fa-solid fa-pencil"></i></button>
-                </section>
-            </section>
-            <section className="ctn-academics">
-                <article className="academic">
-                    <h1>FIAP</h1>
-                    <h2>Bacharelado, Engenharia de Software</h2>
-                    <h3>Fev de 2025 - Dez de 2028</h3>
-                </article>
+            <HeaderCard title='Formação Acadêmica' btnPlus />
 
+            <section className="ctn-academics">
+                {formacao.map((item, index) => (
+                    <div key={index} className="academics">
+                        <article className="academic">
+                            <h1>{item.instituicao}</h1>
+                            <h2>{item.curso}</h2>
+                            <h3>{item.ano}</h3>
+                        </article>
+
+                        {index < formacao.length - 1 && <hr />}
+                    </div>
+                ))}
             </section>
         </section>
-    )
-}
+    );
+};
 
-export default CardAcademic
+export default CardAcademic;
