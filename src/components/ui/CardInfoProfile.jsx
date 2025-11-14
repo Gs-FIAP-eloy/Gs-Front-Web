690c14e5fb09768adc639f4db8c040864560f237
leeosilvp <leo965679@gmail.com>
2025-11-14 04:55:38 -0300

    m- trazendo quantidade de seguidores e seguindo do json do usuario logado para CardInfoProfile

--- src/components/ui/CardInfoProfile.jsx
+++ src/components/ui/CardInfoProfile.jsx
@@ -1,12 +1,38 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const CardInfoProfile = () => {
+
+  const [seguidores, setSeguidores] = useState(0);
+  const [seguindo, setSeguindo] = useState(0);
+
+  useEffect(() => {
+    const user = localStorage.getItem("eloy_user");
+    if (!user) return;
+
+    const usuarioLogado = JSON.parse(user);
+
+    fetch("/db/users.json")
+      .then(res => res.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          const userData = data.find(u => u.id === usuarioLogado.id);
+          
+          if (userData) {
+            setSeguidores(userData.seguidores ?? 0);
+            setSeguindo(userData.seguindo ?? 0);
+          }
+        }
+      })
+      .catch(err => console.error("Erro ao carregar dados:", err));
+  }, []);
+
   return (
     <article className="card-info-profile">
-      <Link to='/profile'>Seguidores: <span>0</span></Link>
-      <Link to='/profile'>Seguindo: <span>0</span></Link>
+      <Link to='/profile'>Seguidores: <span>{seguidores}</span></Link>
+      <Link to='/profile'>Seguindo: <span>{seguindo}</span></Link>
     </article>
-  )
-}
+  );
+};
 
-export default CardInfoProfile
+export default CardInfoProfile;
